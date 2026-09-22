7bba68ca5a6c40928a532bcf4dc4ef5a3d02ba07
Kavya A Y <kavyaay09@gmail.com>
2026-09-22 22:42:59 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    public boolean exist(char[][] board, String word) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word, int i, int j, int index) {
+
+        // Word completely found
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Out of bounds
+        if (i < 0 || i >= board.length ||
+            j < 0 || j >= board[0].length) {
+            return false;
+        }
+
+        // Character doesn't match
+        if (board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        // Mark current cell as visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // Check all 4 directions
+        boolean found =
+            dfs(board, word, i + 1, j, index + 1) ||
+            dfs(board, word, i - 1, j, index + 1) ||
+            dfs(board, word, i, j + 1, index + 1) ||
+            dfs(board, word, i, j - 1, index + 1);
+
+        // Restore the cell
+        board[i][j] = temp;
+
+        return found;
+    }
+}
